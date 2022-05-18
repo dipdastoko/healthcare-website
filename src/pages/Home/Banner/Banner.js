e79fa6f5b81ebf042c2aac0be8e41def381dcad5
dipdastoko <dipdastoko@gmail.com>
2022-05-18 18:16:38 +0600

    banner created

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Banner.css';
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className='text-start ps-5 pt-5'>
+                <h1>Health Care</h1>
+                <h1>Hospital</h1>
+                <p><b><i>Always With You</i></b></p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
