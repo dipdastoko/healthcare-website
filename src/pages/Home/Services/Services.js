237e0487db899abbb6942555d1344980efd12d80
dipdastoko <dipdastoko@gmail.com>
2022-05-24 01:43:08 +0600

    fakedata created

---
+++ src/pages/Home/Services/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2 className='text-start'>Our Services</h2>
+            <hr className='me-5' />
+        </div>
+    );
+};
+
+export default Services;
